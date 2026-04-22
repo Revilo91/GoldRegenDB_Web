7c65a1dd50dbec5e4f8710552c5167463bf8c47a
Revilo91 <oliver.suedholt@gmail.com>
2026-04-22 21:00:42 +0200

    refactor: simplify getPieces function in Lieferscheine and Rechnungen components

--- frontend/src/pages/Rechnungen.jsx
+++ frontend/src/pages/Rechnungen.jsx
@@ -22,12 +22,7 @@ export default function Rechnungen() {
         createItem: api.createRechnung,
         exportExcel: api.exportRechnungExcel,
         getKunden: api.getKunden,
-        getPieces: (filter) => api.getSchmuckstuecke({
-          ausgelagert: filter.Kundennummer,
-          verkauft: "0",
-          ausschuss: "0",
-          limit: -1,
-        }),
+        getPieces: (filter) => api.getSchmuckstuecke(filter),
       }}
       icons={{ header: faFileInvoice, modal: faFileInvoice, user: faUser, trash: faTrash, times: faTimes }}
       labels={{
